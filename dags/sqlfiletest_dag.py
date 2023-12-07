cf2e64d0c37e967d01e100c78e36c53a2d923fa7
h1-the-swan <jason.portenoy@gmail.com>
2023-12-07 12:28:22 -0500

    test sql file

---
+++ dags/sqlfiletest_dag.py
@@ -0,0 +1,21 @@
+from airflow.decorators import task, dag
+from airflow.models import Variable
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+import heroku3
+import requests
+from datetime import datetime, timedelta
+
+import logging
+logger = logging.getLogger(__name__)
+
+@dag(
+    schedule=timedelta(hours=6),
+    start_date=datetime(2023, 9, 25),
+    catchup=False,
+    tags=["test"],
+    template_searchpath="/usr/local/airflow/include",
+)
+def test_sql():
+    SQLExecuteQueryOperator(conn_id="OPENALEX_DB", sql="test.sql")
+
+test_sql()
---
+++ include/test.sql
@@ -0,0 +1,9 @@
+\timing
+
+select * from mid.affiliation limit 1;
+
+select * from authorships.for_disambiguation_work_authors_mv limit 5;
+
+
+refresh materialized view authorships.for_disambiguation_work_authors_mv;
+vacuum analyze authorships.for_disambiguation_work_authors_mv;
