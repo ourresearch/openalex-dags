5229961f07dc334cc4a13d820cd11156c4d27e28
h1-the-swan <jason.portenoy@gmail.com>
2024-01-28 14:02:13 -0500

    refresh other topic-related citation mvs

--- include/refresh-citation-topics-mv.sql
+++ include/refresh-citation-topics-mv.sql
@@ -1,3 +1,11 @@
 refresh materialized view concurrently mid.citation_topics_mv;
 vacuum analyze mid.citation_topics_mv;
 
+refresh materialized view concurrently mid.citation_subfields_mv;
+vacuum analyze mid.citation_subfields_mv;
+
+refresh materialized view concurrently mid.citation_fields_mv;
+vacuum analyze mid.citation_fields_mv;
+
+refresh materialized view concurrently mid.citation_domains_mv;
+vacuum analyze mid.citation_domains_mv;
