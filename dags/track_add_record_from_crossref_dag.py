4fac4b45fd6e3147ade057b9e127a37d69cb87cc
h1-the-swan <jason.portenoy@gmail.com>
2024-02-05 17:20:11 -0500

    look for a new crossref record to track once per day

--- dags/track_add_record_from_crossref_dag.py
+++ dags/track_add_record_from_crossref_dag.py
@@ -4,25 +4,25 @@ import heroku3
 from datetime import datetime, timedelta
 
 # script in openalex-guts to run:
-SCRIPT_TO_RUN = "tracking.scripts.track_all_from_tbl"
+SCRIPT_TO_RUN = "tracking.scripts.add_new_crossref"
 
 
 @dag(
-    schedule=timedelta(hours=1),
-    start_date=datetime(2024, 1, 2),
+    schedule=timedelta(days=1),
+    start_date=datetime(2024, 1, 2, 0, 0, 0),
     catchup=False,
     tags=["recordtrack"],
 )
-def track_records_from_tbl_dag():
+def track_add_record_from_crossref():
     @task()
-    def heroku_run_track_records_from_tbl():
+    def heroku_run_add_new_crossref():
         heroku_api_key = Variable.get("HEROKU_API_KEY")
         heroku_conn = heroku3.from_key(heroku_api_key)
         app = heroku_conn.apps()["openalex-guts"]
         output, dyno = app.run_command(f"python -m {SCRIPT_TO_RUN}")
         return {"output": output}
 
-    result = heroku_run_track_records_from_tbl()
+    result = heroku_run_add_new_crossref()
 
 
-track_records_from_tbl_dag()
+track_add_record_from_crossref()
