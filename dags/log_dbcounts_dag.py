fa06604630483e1abdf120453eb626f86a6740f3
h1-the-swan <jason.portenoy@gmail.com>
2024-07-18 13:14:47 -0400

    trying parameterized dblogs queries

---
+++ dags/log_dbcounts_dag.py
@@ -0,0 +1,45 @@
+from airflow.decorators import task, dag
+from airflow.models import Variable
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from datetime import datetime, timedelta
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+@dag(
+    schedule=timedelta(hours=12),
+    start_date=datetime(2024, 7, 17, 2, 20, 0),
+    catchup=False,
+    tags=["logs", "dblogs"],
+    template_searchpath="/usr/local/airflow/include",
+)
+def log_dbcounts_dag():
+
+    @task
+    def get_tables_to_log():
+        SQLExecuteQueryOperator(
+            task_id="get_tables_to_log",
+            sql="SELECT tablename, schema_name FROM logs.dbcounts_tables_to_track WHERE active IS TRUE AND times_per_day = 2;",
+        )
+
+    @task
+    def count_query(tablename, schema_name):
+        SQLExecuteQueryOperator(
+            task_id=f"execute_query_{schema_name}.{tablename}",
+            autocommit=True,
+            split_statements=True,
+            conn_id="OPENALEX_DB",
+            sql="dbcount-parameterized.sql",
+            params={"tablename": tablename, "schema_name": schema_name},
+        )
+
+    tablenames_query_result = get_tables_to_log()
+    tablenames = [r.tablename for r in tablenames_query_result]
+    schema_names = [r.schema_name for r in tablenames_query_result]
+    # Create, in parallel, one task per result
+    count_query.expand(tablename=tablenames, schema_name=schema_names)
+
+
+log_dbcounts_dag()
---
+++ include/dbcount-parameterized.sql
@@ -0,0 +1,6 @@
+with c as (
+	select count(*) as row_count from {{ params.schema_name }}.{{ params.tablename }}
+)
+insert into logs.dbcount (query_timestamp, row_count, tablename, schema_name)
+	select now() at time zone 'utc', row_count, '{{ params.tablename }}', '{{ params.schema_name }}'
+	from c;
