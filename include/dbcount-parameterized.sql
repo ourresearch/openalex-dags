2cfe6c070c98a735a14a61b941b3bb96b94cabf9
h1-the-swan <jason.portenoy@gmail.com>
2024-07-18 16:05:42 -0400

    rename table

--- include/dbcount-parameterized.sql
+++ include/dbcount-parameterized.sql
@@ -1,6 +1,6 @@
-with c as (
+with count_q as (
 	select count(*) as row_count from {{ params.schema_name }}.{{ params.tablename }}
 )
 insert into logs.dbcount (query_timestamp, row_count, tablename, schema_name)
 	select now() at time zone 'utc', row_count, '{{ params.tablename }}', '{{ params.schema_name }}'
-	from c;
+	from count_q;
